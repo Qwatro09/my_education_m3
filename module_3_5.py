5d5608fe19e7e3d5dd9a21a415f797b48937355f
Андрей <andrepq71q@gmail.com>
2024-11-25 13:03:48 +0300

    Homework_3_5

---
+++ module_3_5.py
@@ -0,0 +1,20 @@
+def get_multiplied_digits(number):
+    if number % 10 == 0:
+        number //= 10
+    str_number = str(number)
+    first = int(str_number[:1])
+    if len(str_number) <= 1:
+        return number
+    else:
+        number = first * get_multiplied_digits(int(str_number[1:]))
+        # print(first, type(first))
+        return number
+
+
+result = get_multiplied_digits(40203)
+
+print(result)
+
+result2 = get_multiplied_digits(402030)
+
+print(result2)
